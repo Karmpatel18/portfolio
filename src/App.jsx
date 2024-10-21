813c726f1009ea5f158e56be594e6d2088e3b3eb
Karmpatel18 <karmpatel1203@gmail.com>
2024-10-21 20:57:51 +0530

    nav-bar added

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex justify-between">
+        <div className="div">
+          <h1>hey there</h1>
+        </div>
+        <div className="div">
+
+          <a href='#'>Home</a>
+          <a href='#'>About</a>
+          <a href='#'>Contact</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
