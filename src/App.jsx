215442dd5035ce6f1f25919f6d3ff5eccc4cb29c
Karmpatel18 <karmpatel1203@gmail.com>
2024-10-22 18:14:21 +0530

    responsice nav-bar

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
 
   return (
     <>
-      <div className="flex justify-between">
-        <div className="div">
+      <div className="font-montserrat font-medium pt-4 flex justify-between flex-col sm:flex-row items-center">
+        <div className="p-4 sm:ml-40">
           <h1>hey there</h1>
         </div>
-        <div className="div">
+        <div className="p-4 space-x-12 sm:mr-40 ">
 
           <a href='#'>Home</a>
           <a href='#'>About</a>
