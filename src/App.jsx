419e8d08b6835ff5c5ee024f62a2f7706891bf95
Karmpatel18 <karmpatel1203@gmail.com>
2024-10-23 14:21:33 +0530

    reponsive component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 // import { useState } from 'react'
-import { Component } from 'react'
 import Box from './components/box'
 
 function App() {
@@ -8,10 +7,10 @@ function App() {
   return (
     <>
       <div className="font-montserrat text-sm font-medium pt-4 flex justify-between flex-col sm:flex-row items-center">
-        <div className="p-4 sm:ml-40">
+        <div className="p-4 md:ml-16  sm:ml-16 lg:ml-40">
           <h1>hey there</h1>
         </div>
-        <div className="p-4 space-x-12 sm:mr-40 ">
+        <div className="p-4 space-x-12 md:mr-16 sm:mr-16 lg:mr-40 ">
 
           <a href='#'>Home</a>
           <a href='#'>About</a>
@@ -19,6 +18,7 @@ function App() {
         </div>
       </div>
       <Box />
+      
     </>
   )
 }
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -5,16 +5,20 @@ function Box() {
   return (
     
       <div className='font-montserrat'>
-        <div className="flex flex-col place-content-between p-10 lg:ml-40 lg:mr-40 m-4 rounded-3xl h-96 w-auto bg-gray-100">
+        <div className="flex flex-col place-content-between p-10 lg:ml-40 lg:mr-40 sm:ml-16 sm:mr-16 m-4 rounded-3xl h-[508px] w-auto bg-gray-100">
+            <div className="div">
             <h1 className='text-gray-900 font-bold text-4xl'>Hello, I’m Abo, a product Designer With 7 years of experience.</h1>
-            <div className="flex sm:flex-row flex-col space-y-2 sm:space-x-6">
-            <button className='bg-gray-900 py-4 px-10 rounded-full text-white text-center  font-semibold'>Contact me</button>
-              <div className="flex flex-row space-x-6">
-              <button className='bg-white rounded-full h-14 w-14 place-content-center'><img className="bg-gray-900 rounded-3xl h-6 w-6 ml-4"src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png?20200512141346" alt="instagram" /></button>
-            <button className='bg-white rounded-full h-14 w-14 place-content-center'><img className="bg-gray-900 rounded-3xl h-6 w-6 ml-4"src="	data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNDgiIGhl…9InNoYXBlIi8+CjwvZmlsdGVyPgo8L2RlZnM+Cjwvc3ZnPgo=" alt="instagram" /></button>
-            <button className='bg-white rounded-full h-14 w-14 place-content-center'><img className="bg-gray-900 rounded-3xl h-6 w-6 ml-4"src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png?20200512141346" alt="instagram" /></button>
-            <button className='bg-white rounded-full h-14 w-14 place-content-center'><img className="bg-gray-900 rounded-3xl h-6 w-6 ml-4"src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1200px-Instagram_icon.png?20200512141346" alt="instagram" /></button>
-              </div>
+            <p className='mt-12'>I care a lot about using design for positive impact. and enjoy creating user-centric, delightful, and human experiences.</p>
+            </div>
+            <div className="flex sm:flex-row space-x-6 flex-col  ">
+              <button className='bg-[#1D1D1D]  px-10 py-3 rounded-full  font-medium text-white text-center '>Contact me</button>
+                <div className="flex items-center  gap-6  ">
+                  <img className="h-[56px] w-[56px]" src="src\assets\twitter.svg" alt="instagram" />                
+                  <img className="h-[56px] w-[56px]" src="src\assets\twitter.svg" alt="instagram" />                
+                  <img className="h-[56px] w-[56px]" src="src\assets\twitter.svg" alt="instagram" />                
+                  <img className="h-[56px] w-[56px]" src="src\assets\twitter.svg" alt="instagram" />                
+
+                </div>  
             </div>
 
         </div>
